f8d669e625c18b362df1929e6fabf3cc48d5ac88
nikfit <nickfit@yandex.ru>
2024-06-19 20:13:50 +0300

    lab1

--- Основы ООП/Лабораторная 1/task1/main.py
+++ Основы ООП/Лабораторная 1/task1/main.py
@@ -1,6 +1,9 @@
 from typing import Union
+import doctest
+
+
 class Car:
-    def __init__(self, hp: Union[int, float], max_speed: Union[int, float]):
+    def __init__(self, hp: Union[int, float], max_speed: Union[int, float], brake_distance: float):
         """
         Создание и подготовка к работе объекта "Автомобиль"
 
@@ -12,13 +15,25 @@ class Car:
         self.hp = hp
         self.max_speed = max_speed
 
+
     def chip_tuning(self, chip: Union[int, float]):
         ...
         """
         Увеличение мощности двигателя
+        :param chip: Процент, на который увеличивается мощность
         :return: Измененная мощность 
+        Примеры:
+        >>> car = Car(150, 200)
+        >>> car.chip_tuning()
         """
+
+
+    def good_brakes(self, ):
+
+
+class
+
 
 if __name__ == "__main__":
     # TODO работоспособность экземпляров класса проверить с помощью doctest
-    pass
+    doctest.testmod()
